dfececeb02cc22fc0dd5cb9c6c1ded7cbd01dcd9
shomurodoff <shoxruxshomurodoff@gmail.com>
2026-04-24 19:02:04 +0500

    fix:onboarding

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import viteImagemin from "vite-plugin-imagemin";
+import compression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
@@ -22,6 +24,13 @@ export default defineConfig({
         quality: 75,
       },
     }),
+    compression({ algorithm: "brotliCompress" }),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
   define: {},
   server: {
@@ -32,6 +41,33 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "esnext",
+    minify: "terser",
+    sourcemap: false,
+    cssCodeSplit: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          qr: ["html5-qrcode", "qrcode"],
+          media: ["recordrtc", "react-media-recorder"],
+          react: ["react", "react-dom", "react-router"],
+          query: ["@tanstack/react-query"],
+          phone: ["react-phone-number-input", "libphonenumber-js"],
+          charts: ["recharts"],
+          pdf: ["jspdf", "html2canvas"],
+          motion: ["framer-motion"],
+        },
+      },
     },
   },
 });
